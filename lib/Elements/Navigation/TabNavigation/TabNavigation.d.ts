71ab1b7a0e8404fc205fc0e46bce1e09456afc2a
SergeiMeza <sergeimeza@gmail.com>
2021-09-13 04:38:39 +0900

    add nav bar

--- lib/Elements/Navigation/TabNavigation/TabNavigation.d.ts
+++ lib/Elements/Navigation/TabNavigation/TabNavigation.d.ts
@@ -3,12 +3,16 @@ import { NavigationItem } from '../NavigationItem';
 export declare type TabNavigationProps = {
     tabs?: NavigationItem[];
     type?: TabNavigationType;
+    showBadge?: boolean;
     showIcons?: boolean;
+    isNavBar?: boolean;
     onTabSelected?: (tab: NavigationItem) => void;
 };
 declare enum TabNavigationType {
     default = "default",
     pills = "pills"
 }
-export declare const TabNavigation: React.FC<TabNavigationProps>;
+export declare const TabNavigation: React.FC<TabNavigationProps> & {
+    type: typeof TabNavigationType;
+};
 export default TabNavigation;
